f8fe2fa0881d92b40c832ef2d841dc698ad28c9a
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-09-12 12:36:03 +0200

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app toolbar footer>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      light
+      enable-resize-watcher
+      absolute
+      clipped
+    >
+      <v-list>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="indigo" dark fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Book Trading Club</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <!--v-router-->
+      </v-container>
+    </main>
+    <v-footer class="indigo">
+      <span class="white--text">© 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true
+      }
+    }
+  }
+</script>
