d874b6dd83fd6729854f785f1377c7a3483e9090
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-09-18 12:48:32 +0200

    Add modal for book picking

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -11,12 +11,12 @@
         ></v-text-field>
       </v-flex>
     </v-layout>
-    <v-layout row>
+    <v-layout row v-if="books.length > 0">
       <v-flex sm6 offset-sm3>
         <v-list>
           <v-list-tile
-            @click=""
-            v-for="book in books"
+            @click="openDialog(i)"
+            v-for="(book, i) in books"
             :key="book.title"
           >
             <v-list-tile-content>
@@ -24,11 +24,22 @@
               <v-list-tile-sub-title>{{ book.volumeInfo.authors[0] }}</v-list-tile-sub-title>
             </v-list-tile-content>
             <v-list-tile-action>
-              <v-icon class="white--text text--lighten-1">add_circle</v-icon>
+              <v-icon class="white--text">add_circle</v-icon>
             </v-list-tile-action>
           </v-list-tile>
         </v-list>
       </v-flex>
+    </v-layout>
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" persistent width="500px">
+        <v-card v-if="pickedBook.info">
+          <v-card-title class="headline">Add "{{ pickedBook.info.volumeInfo.title }}" to your collection?</v-card-title>
+          <v-card-actions>
+            <v-btn class="green--text darken-1" flat="flat" @click.native="dialog = false">Yes</v-btn>
+            <v-btn class="red--text darken-1" flat="flat" @click.native="dialog = false">No</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-layout>
   </div>
 </template>
@@ -40,24 +51,28 @@ export default {
   data () {
     return {
       searchKeyword: '',
-      books: []
+      books: [],
+      APIURL: 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyBbI3lL95nvzfZ6SV3IBQcKOJR2fVdH0sk',
+      dialog: false,
+      pickedBook: {
+        info: null,
+        index: null
+      }
     }
   },
   methods: {
     async getBooks () {
       try {
-        const { data: { items } } = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?key=AIzaSyBbI3lL95nvzfZ6SV3IBQcKOJR2fVdH0sk&q=${this.searchKeyword}`
-        )
+        const { data: { items } } = await axios.get(`${this.APIURL}&q=${this.searchKeyword}`)
         this.books = items
-        console.log(this.books)
       } catch (error) {
         console.log(error)
       }
+    },
+    openDialog (i) {
+      this.pickedBook = { info: this.books[i], index: i }
+      this.dialog = true
     }
-  },
-  created () {
-    console.log(axios.defaults.headers.common)
   }
 }
 </script>
