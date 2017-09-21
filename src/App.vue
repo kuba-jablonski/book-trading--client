55675fee7a053ee134e08ca783c707057ca5f5e4
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-09-21 20:05:53 +0200

    Refactor navigation list as separate component

--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,7 @@
       enable-resize-watcher
       clipped
     >
-      <v-list>
-        <v-list-tile
-          v-for="item in bookItems"
-          :key="item.title"
-          :to="item.path"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>          
-        </v-list-tile>
-        <v-divider></v-divider>
-        <v-list-tile
-          v-for="item in userItems"
-          :key="item.title"
-          :to="item.path"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>          
-        </v-list-tile>
-        <v-list-tile v-if="authenticated" @click="logOut">
-          <v-list-tile-action>
-            <v-icon>arrow_back</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Log out</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>           
-      </v-list>
+      <app-navigation></app-navigation>
     </v-navigation-drawer>
     <v-toolbar class="primary" dark fixed>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
@@ -62,6 +26,8 @@
 </template>
 
 <script>
+import Navigation from './components/Navigation.vue'
+
 export default {
   data () {
     return {
@@ -69,37 +35,15 @@ export default {
     }
   },
   computed: {
-    authenticated () {
-      return this.$store.getters.authenticated
-    },
-    bookItems () {
-      let bookItems = [{ title: 'Browse Books', path: '/', icon: 'home' }]
-      if (this.authenticated) {
-        bookItems = [
-          { title: 'Browse Books', path: '/', icon: 'home', visible: true },
-          { title: 'My Books', path: '/books/me', icon: 'library_books' },
-          { title: 'Add a Book', path: '/books/add', icon: 'library_add' }
-        ]
-      }
-      return bookItems
-    },
-    userItems () {
-      if (!this.authenticated) {
-        return [
-          { title: 'Log In', path: '/user/login', icon: 'forward' },
-          { title: 'Sign Up', path: '/user/signup', icon: 'person_add' }
-        ]
-      }
-    },
     username () {
       return this.$store.getters.username
+    },
+    authenticated () {
+      return this.$store.getters.authenticated
     }
   },
-  methods: {
-    logOut () {
-      this.$store.commit('setUser', null)
-      this.$store.commit('setAuthToken', null)
-    }
+  components: {
+    appNavigation: Navigation
   },
   mounted () {
     this.$store.dispatch('fetchBooks')
