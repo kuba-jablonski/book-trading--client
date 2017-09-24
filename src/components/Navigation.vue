96b68b2ee69257ff5b89d76aadc5290db51f135f
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-09-24 11:41:29 +0200

    Request tab only for authenticated users

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -13,18 +13,20 @@
         <v-list-tile-title>{{ item.title }}</v-list-tile-title>
       </v-list-tile-content>          
     </v-list-tile>
-    <v-divider></v-divider>
-    <v-list-tile
-      to="/requests/me"
-      exact
-    >
-      <v-list-tile-action>
-        <v-icon>chat</v-icon>
-      </v-list-tile-action>
-      <v-list-tile-content>
-        <v-list-tile-title>Requests</v-list-tile-title>
-      </v-list-tile-content>          
-    </v-list-tile>    
+    <template v-if="authenticated">
+      <v-divider></v-divider>
+      <v-list-tile
+        to="/requests/me"
+        exact
+      >
+        <v-list-tile-action>
+          <v-icon>chat</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Requests</v-list-tile-title>
+        </v-list-tile-content>          
+      </v-list-tile> 
+    </template>
     <v-divider></v-divider>
     <v-list-tile
       v-for="item in userItems"
